d56b840e13b050bd7d3075ba96c84df836f0bb7d
davidsomekh <67224401+davidsomekh@users.noreply.github.com>
2022-07-17 07:50:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/solidmetal/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.solidmetal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView m_WebView = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        InitWebView();
+    }
+
+    public void InitWebView()
+    {
+        try {
+            m_WebView = (WebView) findViewById(R.id.webview);
+        }
+        catch (Exception e)
+        {
+            String sm = e.getMessage();
+            e.getMessage();
+        }
+
+        WebSettings webSettings = m_WebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        m_WebView.clearCache(true);
+        m_WebView.setWebViewClient(new WebViewClient());
+        //    m_Webview.loadUrl("file:///android_asset/index.html");
+        m_WebView.loadUrl("https://app.solidmetal.com");
+    }
+}
